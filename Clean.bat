e63530981369761107eecc639c99e3a32c92f66d
logandev <logandvllrd@gmail.com>
2025-05-30 14:04:20 +0200

    remove deleting the imgui.ini file in the Clean.bat

--- Clean.bat
+++ Clean.bat
@@ -8,7 +8,6 @@ del /F /Q cmake_install.cmake
 del /F /Q CMakeCache.txt
 del /F /Q ZERO_CHECK.vcxproj
 del /F /Q ZERO_CHECK.vcxproj.filters
-del /F /Q imgui.ini
 
 rmdir /S /Q _deps
 rmdir /S /Q CHIP8-Emulator.dir
